87eee125b3c175daf49e0612b7c2a81c0b44f253
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-13 16:33:09 -0300

    Add files via upload

---
+++ 1oBI/Aula-3/code04.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // Exemplo 1: Variáveis
+    $nome = "Carlos"; // String
+    $idade = 25;    // Integer
+    $altura = 1.80; // Float
+    $isAtivo = true; // Boolean
+    $nulo = NULL; // NULL
+
+    echo "meu nome é: " . $nome . "<br>";
+    echo "minha idade é: " . $idade . "<br>";    
+    echo "minha altura é: " . $altura . "<br>";
+    echo "estou ativo? " . $isAtivo . "<br>";
+    echo "variável nula: " . $nulo . "<br>";  
+    ?>
+
+</body>
+
+</html>
