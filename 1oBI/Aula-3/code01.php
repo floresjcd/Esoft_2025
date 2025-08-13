a72448054cb6e1626fdc41d98aa93481eb44fa45
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-13 17:02:18 -0300

    Update code01.php

--- 1oBI/Aula-3/code01.php
+++ 1oBI/Aula-3/code01.php
@@ -1,3 +1,10 @@
-<?php
-echo "Olá, mundo!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Primeiro PHP</title>
+</head>
+<body>
+    <h1><?php echo "Bem-vindo ao PHP!"; ?></h1>
+    <p>Este conteúdo foi gerado com PHP.</p>
+</body>
+</html>
