7b1c243275bae9219ef4821f296e61c5b8a380c5
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-19 14:38:14 -0300

    Update code01.php

--- 1oBI/Aula-5/code01.php
+++ 1oBI/Aula-5/code01.php
@@ -8,14 +8,16 @@
     <body>
         <h1> Operadores Aritméticos</h1>
         <?php
-            $num1 = 20;
-            $num2 = 7;
+            $num1 = 5;
+            $num2 = 2;
 
             echo "<p>Soma: " . ($num1 + $num2) . "</p>";
             echo "<p>Subtração: " . ($num1 - $num2) . "</p>";
             echo "<p>Multiplicação: " . ($num1 * $num2) . "</p>";
             echo "<p>Divisão: " . ($num1 / $num2) . "</p>";
-            echo "<p>Módulo: " . ($num1 % $num2) . "</p>";
+            echo "<p>Módulo(resto): " . ($num1 % $num2) . "</p>";
+            echo "<p>Exponenciação: " . ($num1 ** $num2) . "</p>";
+
             ?>
     </body>
 
