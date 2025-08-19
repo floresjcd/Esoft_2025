da1fedd1e9796b3934f73a051d6e243589f32b39
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-19 14:31:57 -0300

    Update code01.php

--- 1oBI/Aula-5/code01.php
+++ 1oBI/Aula-5/code01.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>Meu Primeiro PHP</title>
+    </head>
+
+    <body>
+        <?php
+            $num1 = 20;
+            $num2 = 7;
+
+            echo "<p>Soma: " . ($num1 + $num2) . "</p>";
+            echo "<p>Subtração: " . ($num1 - $num2) . "</p>";
+            echo "<p>Multiplicação: " . ($num1 * $num2) . "</p>";
+            echo "<p>Divisão: " . ($num1 / $num2) . "</p>";
+            echo "<p>Módulo: " . ($num1 % $num2) . "</p>";
+            ?>
+    </body>
+
+</html>
