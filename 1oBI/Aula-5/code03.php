52251137363f250559b884291ff18c2d303f439e
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-19 15:16:03 -0300

    Update code03.php

--- 1oBI/Aula-5/code03.php
+++ 1oBI/Aula-5/code03.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>Introdução ao PHP</title>
+    </head>
+
+    <body>
+        <h1> Operadores de Comparação ou Relacionais</h1>
+            <?php
+                $valor1 = 10;
+                $valor2 = "10";
+
+                echo "<p>Valor1 == Valor2: ";
+                var_dump($valor1 == $valor2); // true (apenas valor)
+                echo "</p>";
+
+                echo "<p>Valor1 === Valor2: ";
+                var_dump($valor1 === $valor2); // false (valor e tipo)
+                echo "</p>";
+
+                echo "<p>Valor1 > 5: ";
+                var_dump($valor1 > 5); // true
+                echo "</p>";
+            ?>
+    </body>
+
+</html>
