7cae3cd16175e5da706499aea540f9b8b1c605b8
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-19 15:52:26 -0300

    Update code04.php

--- 1oBI/Aula-5/code04.php
+++ 1oBI/Aula-5/code04.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>Introdução ao PHP</title>
+    </head>
+
+    <body>
+        <h1> Operador lógico AND</h1>
+            <?php
+            $idade = 20;
+            $temCarteira = true;
+
+            // Usando '&&'
+            if ($idade >= 18 && $temCarteira) {
+                echo "Pode dirigir.\n"; // Será exibido
+            }
+
+            // Usando 'and'
+            if ($idade >= 18 and $temCarteira) {
+                echo "Também pode dirigir.\n"; // Será exibido
+            }
+            ?>
+    </body>
+
+</html>
