0dd4f96e2b012fbe780e13882f2e40206177b51a
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-19 15:04:32 -0300

    Update code02.php

--- 1oBI/Aula-5/code02.php
+++ 1oBI/Aula-5/code02.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>Introdução ao PHP</title>
+    </head>
+
+    <body>
+        <h1> Operadores de Atribuição</h1>
+        <?php
+        
+            $total = 100;
+            $total += 50; // $total agora é 150
+            echo "<p>Total: " . $total . "</p>";
+
+            $mensagem = "Bem-vindo ";
+            $mensagem .= "ao PHP!"; // $mensagem agora é "Bem-vindo ao PHP!"
+            echo "<p>Mensagem: " . $mensagem . "</p>";
+
+            ?>
+    </body>
+
+</html>
