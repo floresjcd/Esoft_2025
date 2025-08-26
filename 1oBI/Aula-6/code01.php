1dbf5f35de29e247697b61bf76a18c7fe15441e2
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2025-08-26 14:48:42 -0300

    Update code01.php

--- 1oBI/Aula-6/code01.php
+++ 1oBI/Aula-6/code01.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>Introdução ao PHP</title>
+    </head>
+
+    <body>
+        <h1> Estruturas Condicionais (if)</h1>
+        <?php
+            $idade = 18;
+
+            if ($idade >= 18) {
+                echo "<p>Você é maior de idade.</p>";
+            } 
+        ?>
+    </body>
+
+</html>
